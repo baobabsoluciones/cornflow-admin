e0e61409d5f79b285e0b7c2b7c054ae100fef0b1
Ampuerin <ampuerin@gmail.com>
2021-02-10 11:37:48 +0100

    Port like var on dockerfile entrypoint

--- initapp.sh
+++ initapp.sh
@@ -3,9 +3,11 @@
 
 # Global defaults and back-compat
 : "${CORNFLOW_URL:="http://localhost:5000"}"
+: "${PORT:="8080"}"
 
 export \
-  CORNFLOW_URL
+  CORNFLOW_URL \
+  PORT
 
 # check if cornflow_url is empty con `.env.cloud`
 if [ -z $(cat /usr/src/app/.env.cloud | grep BASE_URL) ]; then
@@ -13,4 +15,4 @@ echo "VUE_APP_BASE_URL=${CORNFLOW_URL}" >> /usr/src/app/.env.cloud
 fi
 
 # execute serve with mode `cloud`
-npm run cloud
+npm run cloud --port $PORT
