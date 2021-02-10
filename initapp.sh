0fa6a456ec6877b3d850de9a1b0f905b576b73d0
Ampuerin <ampuerin@gmail.com>
2021-02-10 12:31:59 +0100

    delete port var from entrypoint

--- initapp.sh
+++ initapp.sh
@@ -3,11 +3,9 @@
 
 # Global defaults and back-compat
 : "${CORNFLOW_URL:="http://localhost:5000"}"
-: "${PORT:="8080"}"
 
 export \
-  CORNFLOW_URL \
-  PORT
+  CORNFLOW_URL
 
 # check if cornflow_url is empty con `.env.cloud`
 if [ -z $(cat /usr/src/app/.env.cloud | grep BASE_URL) ]; then
@@ -15,4 +13,4 @@ echo "VUE_APP_BASE_URL=${CORNFLOW_URL}" >> /usr/src/app/.env.cloud
 fi
 
 # execute serve with mode `cloud`
-npm run cloud --port $PORT
+npm run cloud
