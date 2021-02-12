3762a7f8e4e7f97edd853a97f5f49ca5010abd04
Franco Peschiera <pchtsp@gmail.com>
2021-02-12 12:38:05 +0100

    send instance-file working, small refactor to login, solution progress working. (#5)

--- src/views/dashboard/main/ExecutionsTable.vue
+++ src/views/dashboard/main/ExecutionsTable.vue
@@ -21,7 +21,7 @@
           <td>{{ exec.config.solver }}</td>
           <td>{{ exec.config.timeLimit }}</td>
           <td>{{ exec.created_at | moment }}</td>
-          <td>{{ exec.finished ? "OK" : "NOK" }}</td>
+          <td>{{ exec.message }}</td>
           <td
           class="justify-center layout px-0"
           >
